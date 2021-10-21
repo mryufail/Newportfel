77c3fb2f2165c90d45d98d6138b57840224b1733
mryufail <92939830+mryufail@users.noreply.github.com>
2021-10-21 22:38:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 son = float(input("Istalgan sonni kiriting: "))
-if son>=0:
+if son > 0:
     print("Musbat son")
+elif son == 0:
+    print("Musbat ham emas manfiy ham emas")
 else:
     print("Manfiy son")
-
-
